390cfb166df9f1ccdf8a638cc176f9ea3dd8c88e
Edlison <bolin.edlison.shen@gmail.com>
2021-07-11 12:01:02 +0800

    v1.0

--- clientA.py
+++ clientA.py
@@ -2,67 +2,158 @@
 # @Date    : 7/5/21 10:58
 # -*- coding:utf-8 -*-
 from flask import Flask, request, render_template
-from hashlib import md5
+from flask_socketio import SocketIO, emit
 import json
 import base64
 import requests
 import rsa
+from Crypto.Cipher import AES
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+AES_key = b'abcdefghabcdefgh'
+ClientB_url = 'http://0.0.0.0:5002'
 
 
 @app.route('/')
 def index():
-    return render_template('ClientA.html')
+    return render_template('ClientA/index.html')
+
+
+@app.route('/chat')
+def chat():
+    return render_template('ClientA/chat.html')
+
+
+@socketio.on('connect', namespace='/chatroom')
+def connect_chatroom():
+    emit('show event', 'server connected.')
 
 
 @app.route('/send', methods=['POST'])
 def send():
-    client_B_url = 'http://127.0.0.1:5002/message/receive'
     data = request.get_data()
     data = json.loads(data)
-    msg = data['msg']
-    # msg = 'This is a msg sent by client A.'  # TODO 可以输入
-    client_A = ClientA()
-    resp = client_A.send_msg(client_B_url, msg)
-    resp_content = json.loads(resp.content)
-    print(resp_content)
-    en_msg = resp_content['data']['en_msg']
-    de_msg = resp_content['data']['de_msg']
-    is_val = resp_content['data']['is_val']
-    return {
-        'code': 0,
-        'data': {
-            'en_msg': en_msg,
-            'de_msg': de_msg,
-            'is_val': is_val
-        }
+    resp = {
+        'data': data['msg']
     }
+    socketio.emit('show event', resp, namespace='/chatroom')
+    clientA = ClientA(AES_key=AES_key, gen_rsa=False)
+    data = clientA.encrypt_msg(data['msg'])
+    resp = requests.post(ClientB_url + '/receive', json=data)
+    print(resp)
+    return 'success'
+
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    data = request.get_data()
+    data = json.loads(data)
+    clientA = ClientA(AES_key=AES_key, gen_rsa=False)
+    de_msg = clientA.decrypt_msg(data['msg'], data['sign'])
+    resp = {
+        'data': de_msg
+    }
+    socketio.emit('show event', resp, namespace='/chatroom')
+    return 'success'
+
+
+@app.route('/file_send', methods=['POST'])
+def upload():
+    file = request.files.get('send_file')
+    file_byte = file.read()
+    file = file_byte.decode('utf-8').strip()
+    print('file ', file)
+    resp = {
+        'data': file
+    }
+    socketio.emit('file event', resp, namespace='/chatroom')
+    clientA = ClientA(AES_key=AES_key, gen_rsa=False)
+    data = clientA.encrypt_msg(file)
+    print(data)
+    resp = requests.post(ClientB_url + '/file_receive', json=data)
+    print(resp)
+    return 'success'
+
+
+@app.route('/file_receive', methods=['POST'])
+def file_receive():
+    data = request.get_data()
+    data = json.loads(data)
+    clientA = ClientA(AES_key=AES_key, gen_rsa=False)
+    de_msg = clientA.decrypt_msg(data['msg'], data['sign'])
+    resp = {
+        'data': de_msg
+    }
+    socketio.emit('file event', resp, namespace='/chatroom')
+    return 'success'
 
 
 class ClientA:
-    def __init__(self):
-        publicKey, privateKey = rsa.newkeys(nbits=512)
-        self.publicKey = publicKey
-        self.privateKey = privateKey
-        self._store_privateKey()
+    def __init__(self, AES_key, gen_rsa=False):
+        self.aes = AES.new(AES_key)
+        if gen_rsa:
+            publicKey, privateKey = rsa.newkeys(nbits=512)
+            self.publicKey = publicKey
+            self.privateKey = privateKey
+            self._store_key()
+        else:
+            self.publicKey, self.privateKey = self._load_key()
 
-    def _store_privateKey(self):
+    def _store_key(self):
+        with open('./key/public_key', 'wb') as f:
+            f.write(self.publicKey.save_pkcs1())
         with open('./key/private_key', 'wb') as f:
             f.write(self.privateKey.save_pkcs1())
 
-    def send_msg(self, other_url: str, msg: str):
-        signature = md5(msg.encode('utf-8')).hexdigest()
-        en_msg = rsa.encrypt(msg.encode('utf-8'), self.publicKey)
-        en_msg = str(base64.b64encode(en_msg), encoding='utf-8')
+    def _load_key(self):
+        with open('./key/public_key', 'rb') as f:
+            publicKey = f.read()
+        publicKey = rsa.PublicKey.load_pkcs1(publicKey)
+        with open('./key/private_key', 'rb') as f:
+            privateKey = f.read()
+        privateKey = rsa.PrivateKey.load_pkcs1(privateKey)
+        return publicKey, privateKey
+
+    def encrypt_msg(self, msg: str):
+        # 签名
+        sign = rsa.sign(msg.encode('utf-8'), self.privateKey, hash_method='MD5')
+
+        # 加密
+        msg = msg.encode('utf-8')
+        en_msg = self.aes.encrypt(self._to_16_bytes(msg))
+        en_sign = self.aes.encrypt(self._to_16_bytes(sign))
+
         data = {
-            'msg': en_msg,
-            'sign': signature
+            'msg': str(base64.b64encode(en_msg), encoding='utf-8'),
+            'sign': str(base64.b64encode(en_sign), encoding='utf-8')
         }
-        header = {'Content-Type': 'json'}
-        resp = requests.post(other_url, json=data, headers=header)
-        return resp
+
+        return data
+
+    def decrypt_msg(self, msg: str, sign: str):
+        msg = base64.b64decode(msg)
+        sign = base64.b64decode(sign)
+        # 解密
+        de_msg = self.aes.decrypt(msg)
+        de_sign = self.aes.decrypt(sign)
+        de_msg = de_msg.decode('utf-8').strip()
+        hash_val = self._validate(de_msg, de_sign)
+
+        print('de_msg ', de_msg)
+        print('de_sign ', de_sign)
+        print('is_val ', hash_val)
+        return de_msg
+
+    def _validate(self, msg: str, sign: bytes):
+        hash_val = rsa.verify(msg.encode('utf-8'), sign, self.publicKey)
+        return hash_val
+
+    def _to_16_bytes(self, data):
+        while len(data) % 16 != 0:
+            data += b' '
+        return data
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001)
--- clientB.py
+++ clientB.py
@@ -1,23 +1,28 @@
 # @Author  : Edlison
 # @Date    : 7/5/21 11:07
 from flask import Flask, request, render_template, send_from_directory
-from hashlib import md5
+from flask_socketio import SocketIO, emit
+from Crypto.Cipher import AES
 import json
 import base64
 import rsa
 import os
+import requests
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+AES_key = b'abcdefghabcdefgh'
+ClientA_url = 'http://0.0.0.0:5001'
 
 
 @app.route('/')
 def index():
-    return render_template('ClientB.html')
+    return render_template('ClientB/index.html')
 
 
 @app.route('/ftp', methods=['GET'])
 def ftp():
-    return render_template('Login.html')
+    return render_template('ClientB/Login.html')
 
 
 @app.route('/ftp/login', methods=['POST'])
@@ -40,7 +45,7 @@ def download():
     os.chdir(path_ftp)
     file_list = os.listdir()
     os.chdir(path_cur)
-    return render_template('Download.html', file_list=file_list)
+    return render_template('ClientB/Download.html', file_list=file_list)
 
 
 @app.route('/ftp/download/<filename>')
@@ -48,45 +53,141 @@ def download_file(filename):
     return send_from_directory('ftp', filename)
 
 
-@app.route('/message/receive', methods=['POST'])
-def receive():
-    client_B = ClientB()
+@app.route('/chat')
+def chat():
+    return render_template('ClientB/chat.html')
+
+
+@socketio.on('connect', namespace='/chatroom')
+def connect_chatroom(message):
+    print('connect ', message)
+    emit('show event', 'server connected.')
+
+
+@app.route('/send', methods=['POST'])
+def send():
     data = request.get_data()
     data = json.loads(data)
-    en_msg = data['msg']
-    sign = data['sign']
-    de_msg = client_B.decode(en_msg)
-    is_val = client_B.validate(de_msg, sign)
-    print('receive successfully msg: {}, validation: {}'.format(de_msg, is_val))
-    return {
-        'code': 0,
-        'data': {
-            'en_msg': en_msg,
-            'de_msg': de_msg,
-            'is_val': is_val
-        }
+    resp = {
+        'data': data['msg']
     }
+    socketio.emit('show event', resp, namespace='/chatroom')
+    clientA = ClientB(AES_key=AES_key, gen_rsa=False)
+    data = clientA.encrypt_msg(data['msg'])
+    resp = requests.post(ClientA_url + '/receive', json=data)
+    print(resp)
+    return 'success'
+
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    data = request.get_data()
+    data = json.loads(data)
+    print(data)
+    clientA = ClientB(AES_key=AES_key, gen_rsa=False)
+    de_msg = clientA.decrypt_msg(data['msg'], data['sign'])
+    resp = {
+        'data': de_msg
+    }
+    socketio.emit('show event', resp, namespace='/chatroom')
+    return 'success'
+
+
+@app.route('/file_send', methods=['POST'])
+def upload():
+    file = request.files.get('send_file')
+    file_byte = file.read()
+    file = file_byte.decode('utf-8').strip()
+    print('file ', file)
+    resp = {
+        'data': file
+    }
+    socketio.emit('file event', resp, namespace='/chatroom')
+    clientA = ClientB(AES_key=AES_key, gen_rsa=False)
+    data = clientA.encrypt_msg(file)
+    print(data)
+    resp = requests.post(ClientA_url + '/file_receive', json=data)
+    print(resp)
+    return 'success'
+
+
+@app.route('/file_receive', methods=['POST'])
+def file_receive():
+    data = request.get_data()
+    data = json.loads(data)
+    clientA = ClientB(AES_key=AES_key, gen_rsa=False)
+    de_msg = clientA.decrypt_msg(data['msg'], data['sign'])
+    resp = {
+        'data': de_msg
+    }
+    socketio.emit('file event', resp, namespace='/chatroom')
+    return 'success'
 
 
 class ClientB:
-    def __init__(self):
-        self.privateKey = self._load_privateKey()
+    def __init__(self, AES_key, gen_rsa=False):
+        self.aes = AES.new(AES_key)
+        if gen_rsa:
+            publicKey, privateKey = rsa.newkeys(nbits=512)
+            self.publicKey = publicKey
+            self.privateKey = privateKey
+            self._store_key()
+        else:
+            self.publicKey, self.privateKey = self._load_key()
 
-    def _load_privateKey(self):
+    def _store_key(self):
+        with open('./key/public_key', 'wb') as f:
+            f.write(self.publicKey.save_pkcs1())
+        with open('./key/private_key', 'wb') as f:
+            f.write(self.privateKey.save_pkcs1())
+
+    def _load_key(self):
+        with open('./key/public_key', 'rb') as f:
+            publicKey = f.read()
+        publicKey = rsa.PublicKey.load_pkcs1(publicKey)
         with open('./key/private_key', 'rb') as f:
             privateKey = f.read()
         privateKey = rsa.PrivateKey.load_pkcs1(privateKey)
-        return privateKey
+        return publicKey, privateKey
 
-    def decode(self, en_msg: str):
-        en_msg = base64.b64decode(en_msg)
-        de_msg = rsa.decrypt(en_msg, self.privateKey)
-        return de_msg.decode()
+    def encrypt_msg(self, msg: str):
+        # 签名
+        sign = rsa.sign(msg.encode('utf-8'), self.privateKey, hash_method='MD5')
 
-    def validate(self, de_msg: str, sign: str):
-        sign_val = md5(de_msg.encode('utf-8')).hexdigest()
-        return sign == sign_val
+        # 加密
+        msg = msg.encode('utf-8')
+        en_msg = self.aes.encrypt(self._to_16_bytes(msg))
+        en_sign = self.aes.encrypt(self._to_16_bytes(sign))
+
+        data = {
+            'msg': str(base64.b64encode(en_msg), encoding='utf-8'),
+            'sign': str(base64.b64encode(en_sign), encoding='utf-8')
+        }
+
+        return data
+
+    def decrypt_msg(self, msg: str, sign: str):
+        msg = base64.b64decode(msg)
+        sign = base64.b64decode(sign)
+        de_msg = self.aes.decrypt(msg)
+        de_sign = self.aes.decrypt(sign)
+        de_msg = de_msg.decode('utf-8').strip()
+        hash_val = self._validate(de_msg, de_sign)
+
+        print('de_msg ', de_msg)
+        print('de_sign ', de_sign)
+        print('is_val ', hash_val)
+        return de_msg
+
+    def _validate(self, msg: str, sign: bytes):
+        hash_val = rsa.verify(msg.encode('utf-8'), sign, self.publicKey)
+        return hash_val
+
+    def _to_16_bytes(self, data):
+        while len(data) % 16 != 0:
+            data += b' '
+        return data
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    socketio.run(app, host='0.0.0.0', port=5002)
